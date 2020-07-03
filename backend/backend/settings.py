0bb349a6e328d254c77cb296437b8a36fa26bc11
MoutiaFazaa <fazaamouty@gmail.com>
2020-07-03 04:38:42 +0000

    create the API using the Django REST framework

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -37,10 +37,13 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo'
+    'corsheaders',            # add this
+    'rest_framework',         # add this 
+    'todo'                    # add this
 ]
 
 MIDDLEWARE = [
+    'corsheaders.middleware.CorsMiddleware',    # add this
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -119,4 +122,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+ #add this 
 
+CORS_ORIGIN_WHITELIST = ['http://localhost:3000']
--- backend/todo/views.py
+++ backend/todo/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+# todo/views.py
 
-# Create your views here.
+from django.shortcuts import render
+from rest_framework import viewsets          # add this
+from .serializers import TodoSerializer      # add this
+from .models import Todo                     # add this
+        
+class TodoView(viewsets.ModelViewSet):       # add this
+  serializer_class = TodoSerializer          # add this
+  queryset = Todo.objects.all()              # add this
